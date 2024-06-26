b9102c2269ccc17b818e7b5bed4240ad4c623b82
kashishkinra42 <92254736+kashishkinra42@users.noreply.github.com>
2024-06-26 10:41:10 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -315,3 +315,111 @@ writeZipFile(zipFilePath, fileName, updatedContent);
 
 
 console.log('Document updated successfully');
+
+
+//--------------------
+
+const fs = require('fs');
+const AdmZip = require('adm-zip');
+var xpath = require('xpath');
+var dom = require('@xmldom/xmldom').DOMParser;
+const xmlSerializer = require('@xmldom/xmldom').XMLSerializer;
+
+const readZipFile = (zipFilePath, fileName) => {
+    const zip = new AdmZip(zipFilePath);
+    const zipEntry = zip.getEntry(fileName);
+    const content = zipEntry.getData().toString('utf8');
+    return content;
+};
+
+const writeZipFile = (zipFilePath, fileName, content) => {
+    const zip = new AdmZip(zipFilePath);
+    zip.updateFile(fileName, Buffer.from(content, 'utf8'));
+    zip.writeZip(zipFilePath);
+};
+
+const replacePlaceholder = (documentContent, jsonContent) => {
+    const doc = new dom().parseFromString(documentContent, 'text/xml');
+    const select = xpath.useNamespaces({ "w": "http://schemas.openxmlformats.org/wordprocessingml/2006/main" });
+    const tags = JSON.parse(jsonContent);
+
+    const processTagValue = (value) => {
+        if (Array.isArray(value)) {
+            return value.map((item, index) => {
+                if (typeof item === 'string') {
+                    return { text: `${index + 1}. ${item}`, highlighted: false };
+                } else if (typeof item === 'object' && item.text) {
+                    return { text: `${index + 1}. ${item.text}`, highlighted: item.highlighted || false };
+                }
+                return { text: '', highlighted: false };
+            });
+        } else if (typeof value === 'object' && value.text) {
+            return [{ text: value.text, highlighted: value.highlighted || false }];
+        }
+        return [{ text: value, highlighted: false }];
+    };
+
+    select("//w:sdt", doc).forEach(node => {
+        const tagNode = select('.//w:tag/@w:val', node)[0];
+        if (tagNode) {
+            const tagName = tagNode.value;
+            if (tags.hasOwnProperty(tagName)) {
+                const textNodes = select('.//w:t', node);
+                if (textNodes.length > 0) {
+                    const processedValues = processTagValue(tags[tagName]);
+
+                    // Clear existing text nodes
+                    textNodes.forEach((textNode, index) => {
+                        if (index === 0) {
+                            textNode.textContent = processedValues[0].text;
+                        } else {
+                            textNode.parentNode.removeChild(textNode);
+                        }
+                    });
+
+                    // Add new text nodes and line breaks
+                    for (let i = 1; i < processedValues.length; i++) {
+                        const breakNode = doc.createElement('w:br');
+                        const newTextNode = doc.createElement('w:t');
+                        newTextNode.textContent = processedValues[i].text;
+                        
+                        if (processedValues[i].highlighted) {
+                            const runProperties = doc.createElement('w:rPr');
+                            const highlightNode = doc.createElement('w:highlight');
+                            highlightNode.setAttribute('w:val', 'yellow'); // Change to desired highlight color
+                            runProperties.appendChild(highlightNode);
+                            newTextNode.parentNode.insertBefore(runProperties, newTextNode);
+                        }
+                        
+                        textNodes[0].parentNode.appendChild(breakNode);
+                        textNodes[0].parentNode.appendChild(newTextNode);
+                    }
+                    
+                    // Highlight the first text node if necessary
+                    if (processedValues[0].highlighted) {
+                        const runProperties = doc.createElement('w:rPr');
+                        const highlightNode = doc.createElement('w:highlight');
+                        highlightNode.setAttribute('w:val', 'yellow'); // Change to desired highlight color
+                        runProperties.appendChild(highlightNode);
+                        textNodes[0].parentNode.insertBefore(runProperties, textNodes[0]);
+                    }
+                }
+            }
+        }
+    });
+
+    const serializer = new xmlSerializer();
+    return serializer.serializeToString(doc);
+};
+
+const zipFilePath = './Test Document.docx';
+const fileName = 'word/document.xml';
+const jsonPath = './myoutput.json';
+
+const documentContent = readZipFile(zipFilePath, fileName);
+const jsonContent = fs.readFileSync(jsonPath, 'utf8');
+
+const updatedContent = replacePlaceholder(documentContent, jsonContent);
+writeZipFile(zipFilePath, fileName, updatedContent);
+
+console.log('Document updated successfully');
