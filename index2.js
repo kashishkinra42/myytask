fc32b6132983f184acbdcc3324406f893c2a94ab
kashishkinra42 <92254736+kashishkinra42@users.noreply.github.com>
2024-06-26 09:43:22 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -326,3 +326,77 @@ writeZipFile(zipFilePath, fileName, updatedContent);
 
 console.log('Document updated successfully');
 
+====================
+
+const fs = require('fs');
+const AdmZip = require('adm-zip');
+var xpath = require('xpath');
+var dom = require('@xmldom/xmldom').DOMParser;
+const xmlSerializer = require('@xmldom/xmldom').XMLSerializer;
+
+const readZipFile = (zipFilePath, fileName) => {
+    const zip = new AdmZip(zipFilePath);
+    const zipEntry = zip.getEntry(fileName);
+    const content = zipEntry.getData().toString('utf8');
+    return content;
+};
+
+const writeZipFile = (zipFilePath, fileName, content) => {
+    const zip = new AdmZip(zipFilePath);
+    zip.updateFile(fileName, Buffer.from(content, 'utf8'));
+    zip.writeZip(zipFilePath);
+}
+
+const replacePlaceholder = (documentContent, jsonContent) => {
+    const doc = new dom().parseFromString(documentContent, 'text/xml');
+    const select = xpath.useNamespaces({ "w": "http://schemas.openxmlformats.org/wordprocessingml/2006/main" });
+    const tags = JSON.parse(jsonContent);
+
+    const createNumberedParagraphs = (doc, value) => {
+        return value.map((item, index) => {
+            const p = doc.createElement('w:p');
+            const r = doc.createElement('w:r');
+            const t = doc.createElement('w:t');
+            t.textContent = `${index + 1}. ${typeof item === 'string' ? item : item.text}`;
+            r.appendChild(t);
+            p.appendChild(r);
+            return p;
+        });
+    };
+
+    select("//w:sdt", doc).forEach(node => {
+        const tagNode = select('.//w:tag/@w:val', node)[0];
+
+        if(tagNode){
+            const tagName = tagNode.value;
+            if(tags.hasOwnProperty(tagName)){
+                const tagValue = tags[tagName];
+                const textNodes = select('.//w:t', node);
+                if(textNodes.length > 0){
+                    const parentNode = textNodes[0].parentNode.parentNode;
+                    const processedValue = createNumberedParagraphs(doc, tagValue);
+                    processedValue.forEach(p => {
+                        parentNode.parentNode.insertBefore(p, parentNode);
+                    });
+                    parentNode.parentNode.removeChild(parentNode); // Remove the original placeholder
+                }
+            }
+        }
+    });
+
+    const serializer = new xmlSerializer();
+    return serializer.serializeToString(doc);
+};
+
+const zipFilePath = './Test Document.docx';
+const fileName = 'word/document.xml';
+const jsonPath = './myoutput.json';
+const content = readZipFile(zipFilePath, fileName);
+
+const documentContent = readZipFile(zipFilePath, fileName);
+const jsonContent = fs.readFileSync(jsonPath, 'utf8');
+
+const updatedContent = replacePlaceholder(documentContent, jsonContent);
+writeZipFile(zipFilePath, fileName, updatedContent);
+
+console.log('Document updated successfully');
